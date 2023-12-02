c23ea9ff9b3bf6583f40db14854ae6d9c91d404b
Miguel Silva <miguelsilva5989@gmail.com>
2023-12-02 14:13:56 +0100

    almost :)

--- 2023/day-02/src/bin/part1.rs
+++ 2023/day-02/src/bin/part1.rs
@@ -40,7 +40,7 @@ impl GameSet {
 }
 
 #[derive(Debug)]
-struct Game {
+struct Games {
     num: usize,
     game_sets: Vec<GameSet>,
 }
@@ -84,24 +84,46 @@ fn parse_game_sets(input: &str) -> IResult<&str, Vec<GameSet>> {
     Ok(separated_list1(tag(";"), game_set)(input))?
 }
 
-fn parse_line(input: &str) -> IResult<&str, Game> {
+fn parse_line(input: &str) -> IResult<&str, Games> {
     let (input, (_, _, game_number, _, game_sets)) = tuple((tag("Game"), multispace0, digit1, tag(":"), parse_game_sets))(input)?;
     Ok((
         input,
-        Game {
+        Games {
             num: game_number.parse::<usize>().unwrap(),
             game_sets,
         },
     ))
 }
 
-fn part1(input: &str) -> &str {
-    for line in input.lines() {
-        let parsed_line = parse_line(line);
-        dbg!(parsed_line);
+fn check_cube_sum(games: Games) -> (usize, bool) {
+    let mut game_set_sum = GameSet::new();
+
+    for game_set in games.game_sets {
+        game_set_sum.red += game_set.red;
+        game_set_sum.green += game_set.green;
+        game_set_sum.blue += game_set.blue;
     }
 
-    "asd"
+    println!("{:?}", game_set_sum);
+
+    if game_set_sum.red <= 12 && game_set_sum.green <= 13 && game_set_sum.blue <= 14 {
+        return (games.num, true);
+    }
+
+    (0, false)
+}
+
+fn part1(input: &str) -> usize {
+    let mut game_numbers: usize = 0;
+    for line in input.lines() {
+        let (_, games) = parse_line(line).unwrap();
+        let (game_num, result) = check_cube_sum(games);
+        if result {
+            game_numbers += game_num
+        };
+    }
+
+    game_numbers
 }
 
 #[cfg(test)]
@@ -117,6 +139,6 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         );
-        assert_eq!(result, "8");
+        assert_eq!(result, 8);
     }
 }
